d70dccadac87e523866c5283e7d30660fd7c4c19
Shrungar Dadarkar <shrungar@IN-MB-777.local>
2026-07-19 13:41:31 +0530

    Fix wrapped solo event dragging

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "couch-batata-v28";
+const CACHE_NAME = "couch-batata-v29";
 const APP_URL = new URL("./index.html", self.registration.scope).href;
 const APP_SHELL = [
   "./index.html",

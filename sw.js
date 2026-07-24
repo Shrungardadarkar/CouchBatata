96ec20d40ab1cdbcd84be5d1eea421f1b06973da
Shrungar Dadarkar <shrungar@IN-MB-777.local>
2026-07-24 14:44:32 +0530

    Refine masthead and picker states

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "couch-batata-v83";
+const CACHE_NAME = "couch-batata-v84";
 const APP_URL = new URL("./index.html", self.registration.scope).href;
 const APP_SHELL = [
   "./index.html",

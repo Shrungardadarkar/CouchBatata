3b45fbd8e8bb3672b73e57ffda007576cc495610
Shrungar Dadarkar <shrungar@IN-MB-777.local>
2026-07-24 15:02:22 +0530

    Prevent picker menu overlaps

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "couch-batata-v84";
+const CACHE_NAME = "couch-batata-v85";
 const APP_URL = new URL("./index.html", self.registration.scope).href;
 const APP_SHELL = [
   "./index.html",
